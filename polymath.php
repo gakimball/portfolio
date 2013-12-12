4f8d3d26e7be5d2bf7f588f775711e8e410aaea1
Geoff Kimball <gakimball@bsu.edu>
2013-12-11 23:36:01 -0500

    Add boilerplate for case study page

---
+++ polymath.php
@@ -0,0 +1,20 @@
+<? include 'includes/header.php'; ?>
+
+<div class="case-study-header">
+  <h2>
+    Polymath
+    <span>The design process</span>
+  </h2>
+</div>
+
+<div class="case-study-body">
+  <img src="img/work/polymath/01.jpg" class="thumb" alt="Screenshot of Polymath's about page.">
+
+  <p><i>Note: Design is a collaborative, multidisciplinary effort. Although most of this design and all of the code is all me, I had input from my cohorts at work, collaborators on the Polymath project, and a handful of programmers who know their way around the web development world in ways I don't.</i></p>
+
+  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Recusandae, autem possimus nam quis aliquid vitae qui illo doloremque aliquam ab est quaerat omnis incidunt repudiandae distinctio officia quo doloribus deleniti.</p>
+
+  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magni, porro, at, ipsam ipsa quae fuga nam culpa veritatis quibusdam praesentium maxime mollitia adipisci ratione rerum similique nihil facere laudantium quam!</p>
+</div>
+
+<? include 'includes/footer.php'; ?>
