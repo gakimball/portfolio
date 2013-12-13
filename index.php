70a2e3552467ff311c0c4c2767c47c3f5aa49388
Geoff Kimball <gakimball@bsu.edu>
2013-12-13 01:31:23 -0500

    Add links to portfolio list

--- index.php
+++ index.php
@@ -35,8 +35,8 @@
 
         <p>Polymath is a group of musicians, filmmakers, and writers based mostly in Muncie and Bloomington, Indiana, releasing their works online for free. I designed the logo and the website, and I wrote and maintain the server-side codebase as well.</p>
 
-        <a href="" class="button">Read case study &rarr;</a>
-        <a href="" class="button">Visit site &rarr;</a>
+        <a href="polymath.php" class="button">Read case study &rarr;</a>
+        <a href="http://polymatharts.com" class="button" target="_blank">Visit site &rarr;</a>
       </div>
     </section>
 
@@ -50,8 +50,8 @@
 
         <p>I interned at product design firm ZURB this summer, where my main project was redesigning Playground, one of ZURB’s most popular properties which is host to various design resources and technical experiments.</p>
 
-       <a href="" class="button">Read case study &rarr;</a>
-        <a href="" class="button">Visit site &rarr;</a>
+        <a href="playground.php" class="button">Read case study &rarr;</a>
+        <a href="http://zurb.com/playground" class="button" target="_blank">Visit site &rarr;</a>
       </div>
     </section>
 
@@ -65,7 +65,7 @@
 
         <p>I wrote the markup and stylesheets for an admin interface for Ball State University’s rec center employees and staff. I also provided some input on the layout and visual design.  A well-done responsive design was a key goal for this project.</p>
 
-        <a href="" class="button">Read case study &rarr;</a>
+        <a href="bsurec.php" class="button">Read case study &rarr;</a>
      </div>
     </section>
 
@@ -91,5 +91,33 @@
   </section>
 
 </section>
+
+<script>
+  function shuffle(array) {
+    var currentIndex = array.length
+    , temporaryValue
+    , randomIndex
+    ;
+    while (0 !== currentIndex) {
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+      temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
+    }
+    return array;
+  }
+
+  (function(){
+    var words = ['affordance', 'accessibility', 'responsive design', 'content', 'visual design', 'user experience', 'crust', 'sauce', 'cheese', 'meats', 'vegetables', 'pepperoni', 'sausage', 'bacon', 'fur', 'ears', 'tail', 'paws'];
+    wordList = shuffle(words);
+
+    for (var i = 0; i < words.length; i++) {
+      var elem = document.createElement('span');
+      elem.text = wordList[i];
+      console.log(elem);
+    }
+  })();
+</script>
 
 <? include 'includes/footer.php'; ?>
