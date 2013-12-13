1d134fa7482597150aa28bda5275f3f8e57d1b45
Geoff Kimball <gakimball@bsu.edu>
2013-12-13 01:30:44 -0500

    Update Polymath case study

--- polymath.php
+++ polymath.php
@@ -10,11 +10,55 @@
 <div class="case-study-body">
   <img src="img/work/polymath/01.jpg" class="thumb" alt="Screenshot of Polymath's about page.">
 
-  <p><i>Note: Design is a collaborative, multidisciplinary effort. Although most of this design and all of the code is all me, I had input from my cohorts at work, collaborators on the Polymath project, and a handful of programmers who know their way around the web development world in ways I don't.</i></p>
+  <p><i>Design is a collaborative, multidisciplinary effort. Although most of this design and all of the code is all me, I had input from my cohorts at work, collaborators on the Polymath project, and a handful of programmers who know their way around the web development world in ways I don't.</i></p>
 
-  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Recusandae, autem possimus nam quis aliquid vitae qui illo doloremque aliquam ab est quaerat omnis incidunt repudiandae distinctio officia quo doloribus deleniti.</p>
+  <p>Polymath is a project I started with some of my friends last year. We started with an unusual premise: create a music site (more or less a label) where albums are released one track at a time, once a week. On top of that, rope in multiple artists and have them release on a schedule: one every Monday, one every Wednesday, and so on. I built a basic site with little planning in about 4 weeks, using the Django framework, and having never written a line of Python. But it worked out, and I went on to design a second version of the site, and now a third one which you see now.</p>
 
-  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magni, porro, at, ipsam ipsa quae fuga nam culpa veritatis quibusdam praesentium maxime mollitia adipisci ratione rerum similique nihil facere laudantium quam!</p>
+  <h3>The Site</h3>
+
+  <p>We've expanded the scope of the project to include videos and writing, and maybe eventually a podcast series. One of the biggest challenges with this design was finding a way to take these disparate media types and create a site that binds them all together with a common look and feel.</p>
+
+  <img src="img/work/polymath/03.jpg" alt="" class="thumb">
+
+  <p>As you can see it's very clearly "flat design"; my intent was for the site to be a blank canvas for the content that it hosts. Most of the site is black on white, and there isn't even one color scheme; each medium has its own primary color, which is only seen in that section.</p>
+
+  <p>Initially we had slightly more grand aspirations for the visual design, the most grand of which being a very heavy use of giant, full-bleed background images to compliment every piece of content. My initial visual mockups went all-in on this idea. It sure is appealing on the surface, but I eventually pushed back in the other direction. Of course, you can find plenty of places on the web that use large cover images (Medium, every designer's personal blog, etc.), but my feeling was that they would become impractical to maintain for a number of reasons.</p>
+
+  <p>First, it's just hard to find giant images that will scale well. From a responsive design perspective, you aren't guaranteed a nice 4:3-ish viewport; the user could be using a portrait phone or tablet. Then what do you do with that image? And oh, the load times for those big images! But from a more... <em>philosophical</em> perspective, at some point I had to step back and ask "what's the point"? At what point is that giant cover photo just decoration and when is it actually enhancing the content itself? It may just be detracting from it, because let's also talk about what a pain it is to place readable type over an image with uneven colors. I did find some places I felt giant imagery were appropriate, but I reigned things in and didn't let images stretch to the sides of the screen.</p>
+
+  <img src="img/work/polymath/02.jpg" alt="" class="thumb">
+
+  <p>Unfortunately scaling back that aspect of the design left me with a design that I feel is sparse in many places. The end product was a partial rush job; school, work, and this project all combined in a way that forced me to pick and choose what I had time to do. The site is still quite literally half done, as only the music and video sections are open. The writing section should be open by the end of 2013, and the audio section whenever that podcast we're planning happens.</p>
+
+  <h4>The Audio Player</h4>
+
+  <p>One of the site's core features, there since the beginning, is the audio player fixed at the bottom of the page, which follows you around the site. Building a responsive audio player is a tricky challenge, from a design and technical perspective. Building an entire <em>site</em> full of clickable links that seamlessly load music into that player is an even trickier challenge.</p>
+
+  <h3>The Logo</h3>
+
+  <p>I designed Polymath's logo, which debuted with this third version of the site; prior to that the project had no logo.</p>
+
+  <p>I didn't feel like a wordmark would suit the project well, so I wanted to create a symbol unique enough that people recognize it, and hopefully simple enough that it would scale well and work in different colors. When I started coming up with ideas this past summer, I was really fixated on the concept of symbolism. I wanted the logo to have a meaning you could pick apart and explain. Many of my first tries were based around this concept.</p>
+
+  <p>Many of the early concepts revolved around circles, such as the one you see below. This specific design utilizes three different sized circles which all overlap. The smallest circles represent the people who contribute to Polymath; the middle circles represent the four mediums (music, video, writing, audio); and the large center circle represents the project itself, which binds the mediums and the people together.</p>
+
+  <img src="img/work/polymath/06.jpg" alt="" class="thumb">
+
+  <p>At the time I was really into this concept, but my execution left a lot to be desired. The colors I used were dull and get really muddled when they mix together. Additionally, you could probably only display this logo in its full-color version; you can't make it grayscale or single-color.</p>
+
+  <img src="img/work/polymath/07.jpg" alt="" class="thumb">
+
+  <p>Let's call the above "meandering".</p>
+
+  <p>My next "breakthrough" was what I called the "gem logo". The site has always used heart symbols here and there; the very first code I wrote for the site was building a share modal (haha, share buttons) which used a heart symbol with the phrase "share the love". I was also really into this logo for a while, especially paired with Futura or Europa, but I couldn't get anyone else on board.</p>
+
+  <img src="img/work/polymath/08.jpg" alt="" class="thumb">
+
+  <p>But we continued working with the heart motif. A friend of mine on the project sent me the above logo, which incorporates the initials "PM" into a heart shape. I was intrigued by this idea and tried making something similar. At this point I had gotten over my symbolism fetish, and just wanted something simple.</p>
+
+  <p>Eventually I landed on the idea of the "P heart", which just builds the letter P into the logo. I really liked this idea because it incorporates a very simple element of the site (the name) into the heart motif, because we're all about sharing the love of art. The simplicity of this symbol appealed to me, and my friends were into it as well, so I worked on fine-tuning the concept.</p>
+
+  <p>I'm very pleased with the end result, and it was a fun project to take on, but oh boy am I definitely <em>not</em> a logo designer.</p>
 </div>
 
 <? include 'includes/footer.php'; ?>
