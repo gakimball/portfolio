8888754bd628ac273a2616b667c8995a03e2d8e1
Geoff Kimball <gakimball@bsu.edu>
2014-01-27 13:41:31 -0500

    Add responsive modular scale gem

--- config.rb
+++ config.rb
@@ -1,6 +1,7 @@
 # Require any additional compass plugins here.
 require 'susy'
 require 'modular-scale'
+require 'responsive-modular-scale'
 
 # Set this to the root of your project when deployed:
 http_path = "/"
