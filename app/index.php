c6a87397850da09d2e730628e90d92fdc85d8307
Geoff Kimball <gakimball@bsu.edu>
2014-02-03 14:21:48 -0500

    Homepage: add RMS and Teaching on the Edge, other tweaks

--- app/index.php
+++ app/index.php
@@ -10,16 +10,10 @@
       </p>
       <p class="tagline-after">are all I think about.</p>
 
-      <a href="" class="button">Portfolio</a>
       <a href="" class="button" data-resume-open>Resume</a>
+      <a href="#about" class="button">About me</a>
     </div>
     <div class="homepage-hero-pictures">
-      <!-- <div class="icon-container">
-        <img src="img/icons/design.svg" alt="Design" class="icon-1">
-        <img src="img/icons/pizza.svg" alt="Pizza" class="icon-2">
-        <img src="img/icons/cat.svg" alt="Cats" class="icon-3">
-        <img src="img/icons/design.svg" alt="Design" class="icon-4">
-      </div> -->
       <div class="sketch-container">
         <img src="img/icons/design-sketch.png" class="sketch-1" alt="">
         <img src="img/icons/pizza-sketch.png" class="sketch-2" alt="">
@@ -38,16 +32,16 @@
 
     <section class="portfolio-item">
       <div class="portfolio-item-thumb">
-        <img src="img/work/polymath.jpg">
+        <img src="img/work/tote.jpg">
       </div>
       <div class="portfolio-item-content">
-        <h3>Polymath</h3>
-        <p class="subtitle">web + mobile + logo</p>
+        <h3>Teaching on the Edge</h3>
+        <p class="subtitle">web</p>
 
-        <p>Polymath is a group of musicians, filmmakers, and writers based mostly in Muncie and Bloomington, Indiana, releasing their works online for free. I designed the logo and the website, and I wrote and maintain the server-side codebase as well.</p>
+        <p>I designed and built a site for "Teaching on the Edge", a National Science Foundation grant given to Ball State University and Taylor University about teaching the science behind high-altitude ballooning in college and middle school classrooms. Of particular note in this project is an interactive flight sim I built to display GPS and weather data from balloon launches.</p>
 
-        <a href="polymath.php" class="button">Read case study &rarr;</a>
-        <a href="http://polymatharts.com" class="button" target="_blank">Visit site &rarr;</a>
+        <a href="http://ilearn.bsu.edu/tote/" class="button">Visit site &rarr;</a>
+        <a href="http://ilearn.bsu.edu/tote/flights/spring2011.php" class="button" target="_blank">View flight sim &rarr;</a>
       </div>
     </section>
 
@@ -57,7 +51,7 @@
       </div>
       <div class="portfolio-item-content">
         <h3>ZURB Playground</h3>
-        <p class="subtitle">web + mobile</p>
+        <p class="subtitle">web</p>
 
         <p>I interned at product design firm ZURB this summer, where my main project was redesigning Playground, one of ZURB’s most popular properties which is host to various design resources and technical experiments.</p>
 
@@ -68,11 +62,41 @@
 
     <section class="portfolio-item">
       <div class="portfolio-item-thumb">
+        <img src="img/work/rms.jpg">
+      </div>
+      <div class="portfolio-item-content">
+        <h3>Responsive Modular Scale</h3>
+        <p class="subtitle">open source</p>
+
+        <p>I developed a responsive adaption of the Sass team's <a href="https://github.com/Team-Sass/modular-scale" target="_blank">modular scale</a> extension for Compass. This gem allows you to easily define font sizes on a modular scale, while also having your type respond to changes in screen size.</p>
+
+        <a href="http://gakimball.github.io/responsive-modular-scale/" class="button">View demo &rarr;</a>
+        <a href="https://github.com/gakimball/responsive-modular-scale" class="button" target="_blank">View GitHub repo &rarr;</a>
+      </div>
+    </section>
+
+    <section class="portfolio-item">
+      <div class="portfolio-item-thumb">
+        <img src="img/work/polymath.jpg">
+      </div>
+      <div class="portfolio-item-content">
+        <h3>Polymath</h3>
+        <p class="subtitle">web + logo</p>
+
+        <p>Polymath is a group of musicians, filmmakers, and writers based mostly in Muncie and Bloomington, Indiana, releasing their works online for free. I designed the logo and the website, and I wrote and maintain the server-side codebase as well.</p>
+
+        <a href="polymath.php" class="button" target="_blank">Read case study &rarr;</a>
+        <a href="http://polymatharts.com" class="button" target="_blank">Visit site &rarr;</a>
+      </div>
+    </section>
+
+    <section class="portfolio-item">
+      <div class="portfolio-item-thumb">
         <img src="img/work/bsurec.jpg">
       </div>
       <div class="portfolio-item-content">
         <h3>Ball State Recreation</h3>
-        <p class="subtitle">web + mobile</p>
+        <p class="subtitle">web</p>
 
         <p>I wrote the markup and stylesheets for an admin interface for Ball State University’s rec center employees and staff. I also provided some input on the layout and visual design.  A well-done responsive design was a key goal for this project.</p>
 
@@ -86,7 +110,7 @@
 
   </section>
 
-  <section class="about">
+  <section class="about" id="about">
     <h2>About me</h2>
 
     <div class="about-photo">
@@ -94,9 +118,7 @@
     </div>
 
     <div class="about-bio">
-      <p>My name is <i>Geoff Kimball</i>, and I'm a soon-to-be-graduating senior at Ball State University in Muncie, IN. At my current job at BSU's Emerging Technologies department, I contribute to the design and development of responsive websites, desktop and mobile web apps, and native apps. I'm interested in the ways we incorporate technology into our lives. My ambition is to work on products and services that affect millions of people.</p>
-
-      <p>I'm a certified archmage in CSS; I believe pencil (or Sharpie) and paper are the best tools to start a design project with; I know the semantic difference between <code>&lt;i&gt;</code> and <code>&lt;em&gt;</code>, and what <code>vertical-align</code> is actually for; I appreciate flat design but also enjoy a well-placed box shadow; I like Mario, but what I love most are cats.</p>
+      <p>My name is <i>Geoff Kimball</i>, and I'm a soon-to-be-graduating senior at Ball State University in Muncie, IN. At my current job at BSU's Emerging Technologies department, I contribute to the design and development of responsive websites, desktop and mobile web apps, and native apps. Designing or coding, I just want to make awesome things for people. I'm interested in the ways we incorporate technology into our lives, and my ambition is to work on products and services that affect millions of people.</p>
 
       <p><a href="#" data-resume-open>Here's my resume again.</a> <em>And hey, I'm looking for a full-time position!</em></p>
 
